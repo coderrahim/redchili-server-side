858874ea64cc05b809011e33075c2034e416e6af
coderrahim <coderrahimbd@gmail.com>
2023-11-05 17:26:43 +0600

    Food Add & Update  to MongoDB

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // MIDDLEWARE
@@ -14,37 +14,88 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@rahim.i
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
 
-    
+
     try {
 
-      const userCollection = client.db("RedChiliRestaurent").collection("users");
+        const userCollection = client.db("RedChiliRestaurent").collection("users");
+        const foodCollection = client.db("RedChiliRestaurent").collection("addFood");
 
 
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        const result = await userCollection.insertOne(user)
-        res.send(result)
-    })
+        // USER ADD
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // ADD FOOD
+
+        app.get('/addfood/:id', async(req, res) => {
+            const id = req.params.id; 
+            const query = {_id: new ObjectId(id)}
+            const result = await foodCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.get('/addfood', async (req, res) => {
+            let query = {};
+            if(req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await foodCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post('/addfood', async (req, res) => {
+            const food = req.body;
+            const result = await foodCollection.insertOne(food)
+            res.send(result)
+        })
+
+        app.put('/addFood/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updatedFood = req.body;
+
+            const options = {upsert: true}
+
+            const food = {
+                $set: {
+                    name:updatedFood.name,
+                    image:updatedFood.image,
+                    category:updatedFood.category,
+                    quantity:updatedFood.quantity,
+                    price:updatedFood.price,
+                    addedby:updatedFood.addedby,
+                    country:updatedFood.country,
+                    description:updatedFood.description,
+                }
+            }
+            const result = await foodCollection.updateOne(filter, food, options)
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
